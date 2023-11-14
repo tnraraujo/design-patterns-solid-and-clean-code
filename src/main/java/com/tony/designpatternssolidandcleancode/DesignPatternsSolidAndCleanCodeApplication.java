2b192e3d7fb2f089308b6e8cf4ed06d113eaab01
= <=>
2023-11-13 23:58:12 -0300

    Study Design Patterns, Solid and Clean Code

---
+++ src/main/java/com/tony/designpatternssolidandcleancode/DesignPatternsSolidAndCleanCodeApplication.java
@@ -0,0 +1,13 @@
+package com.tony.designpatternssolidandcleancode;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesignPatternsSolidAndCleanCodeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DesignPatternsSolidAndCleanCodeApplication.class, args);
+	}
+
+}
